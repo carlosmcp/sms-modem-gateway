3005c6f328fe3f1bf27a1aa1f9d74da73fb07cbb
carlosmcp <carlosmcp@gmail.com>
2020-05-27 13:40:50 -0300

    fixture number

--- src/__mocks__/Fixtures.ts
+++ src/__mocks__/Fixtures.ts
@@ -4,6 +4,34 @@
  */
 import { Status } from '../GatewayModemSMS'
 
-export const phone = 11999999999
-export const statusErros = Object.keys(Status).filter(item => item !== 'OK')
+export const phone = 5511999999999
+export const statusErros = [{
+  status: Status.ERROR_CONFIG_NOT_FOUND,
+  message: 'Warning: No configuration file found! Warning: No configuration read, using builtin defaults! Error opening device, it doesn\'t exist.'
+}, {
+  status: Status.ERROR_DEVICE_NOT_FOUND,
+  message: 'Error opening device, it doesn\'t exist'
+},
+{
+  status: Status.ERROR_GAMMU_NOT_INSTALLED,
+  message: '/bin/sh: 1: gammu: not found'
+},
+{
+  status: Status.ERROR_OPEN_DEVICE_PERMISSION,
+  message: 'Error opening device, you don\'t have the required permission'
+},
+{
+  status: Status.ERROR_SIM_CARD,
+  message: 'failed to get smsc number from phone'
+},
+{
+  status: Status.ERROR_SEND,
+  message: 'waiting for network answer..error'
+},
+{
+  status: Status.ERROR_UNACKNOWLEDGED,
+  message: ''
+}]
+
 export const message = 'Hello there!'
+export const infraErroMessage = 'unknow error'
